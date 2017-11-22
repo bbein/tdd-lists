c50b9eceabed256b3636a1bb47916aecca8dbe93
Benjamin <benjamin.bein.phd@gmail.com>
2017-11-22 15:43:23 -0500

    Initial commit

---
+++ functional_test.py
@@ -0,0 +1,10 @@
+"""
+Functional tests for the TDD Book
+"""
+
+from selenium import webdriver
+
+browser = webdriver.Firefox()
+browser.get('http:\\localhost:8000')
+
+assert 'Django' in browser.title
