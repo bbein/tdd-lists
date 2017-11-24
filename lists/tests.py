8585b9ef724829e0b5497250fad4bbb2dac8428f
Benjamin <benjamin.bein.phd@gmail.com>
2017-11-24 12:56:16 -0500

    functional test now checks we can input a to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -9,12 +9,20 @@ from django.http import HttpRequest
 from lists.views import home_page
 
 class HomePageTest(TestCase):
-
+    """
+    Unit-tests for the home page
+    """
     def test_root_url_resolves_to_home_page_view(self):
+        """
+        tests that root url resolves to home page view
+        """
         found = resolve('/')
         self.assertEqual(found.func, home_page)
 
     def test_home_page_returns_correct_html(self):
+        """
+        tests that home page returns the correct html
+        """
         request = HttpRequest()
         response = home_page(request)
         html = response.content.decode('utf8')
