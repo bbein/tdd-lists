a42292cf73c64222725a0e69458eacb42e268bbf
Benjamin <benjamin.bein.phd@gmail.com>
2017-11-24 13:32:23 -0500

    Front page html now generated from a template

--- functional_test.py
+++ functional_test.py
@@ -51,7 +51,8 @@ class NewVisitorTest(unittest.TestCase):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertTrue(
-            any(row.text == "1: Buy peacock feathers" for row in rows)
+            any(row.text == "1: Buy peacock feathers" for row in rows),
+            "New to-do item did ot appear in table"
         )
         # There is still a text box inviting her o add another item.
         # She enters "Use peacock feather to make a fly" (Edith is very methodical)
