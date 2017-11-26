0a6737ab51cbfe1146e6edf4e413cedc7a064dd2
Benjamin <benjamin.bein.phd@gmail.com>
2017-11-26 16:35:46 -0500

    implemented the capability to add the first to-do item

--- lists/tests.py
+++ lists/tests.py
@@ -9,10 +9,19 @@ class HomePageTest(TestCase):
     Unit-tests for the home page
     """
 
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         """
         tests that root url resolves to home page view
         and that the home page returns the correct html
         """
         response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """
+        tests that the homepage can save a POST request
+        """
+        item_text = 'A new list item'
+        response = self.client.post('/', data={'item_text': item_text})
+        self.assertIn(item_text, response.content.decode())
         self.assertTemplateUsed(response, 'home.html')
--- lists/views.py
+++ lists/views.py
@@ -1,4 +1,11 @@
+"""
+Homepage Views for the list App
+"""
+
 from django.shortcuts import render
 
 def home_page(request):
-    return render(request, 'home.html')
+    """
+    The main homepage view
+    """
+    return render(request, 'home.html', {'new_item_text': request.POST.get('item_text', '')})
