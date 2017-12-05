60bd064fee12d9aa11964cc37d2bc3f8d54079aa
Benjamin <benjamin.bein.phd@gmail.com>
2017-12-04 22:43:37 -0500

    Urls for dorms x2, moved code in views+ tests, new URL

--- lists/tests.py
+++ lists/tests.py
@@ -10,7 +10,7 @@ class HomePageTest(TestCase):
     """
     Unit-tests for the home page
     """
-    
+
     def test_uses_home_template(self):
         """
         tests that root url resolves to home page view
@@ -18,32 +18,6 @@ class HomePageTest(TestCase):
         """
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_can_save_a_POST_request(self):
-        """
-        tests that the homepage can save a POST request
-        """
-        item_text = 'A new list item'
-        response = self.client.post('/', data={'item_text': item_text})
-        self.assertEqual(Item.objects.count(), 1)
-        new_item = Item.objects.first()
-        self.assertEqual(new_item.text, item_text)
-
-    def test_redirect_after_a_post(self):
-        """
-        tests that the view redirects after it did send a POST request
-        """
-        item_text = 'A new list item'
-        response = self.client.post('/', data={'item_text': item_text})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
-
-    def test_only_save_items_when_necessary(self):
-        """
-        tests that no item is saved in the database if we don't send one
-        """
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
 
 class ItemModelTest(TestCase):
     """
@@ -76,9 +50,9 @@ class ListViewTest(TestCase):
     """
     def test_uses_list_template(self):
         """
-        
+        tests that the user list view uses the list template
         """
-        response =self.client.get('/lists/the-only-list-in-the-world/')
+        response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
 
     def test_display_all_items(self):
@@ -92,3 +66,26 @@ class ListViewTest(TestCase):
 
         self.assertContains(response, 'itemey 1')
         self.assertContains(response, 'itemey 2')
+
+class NewListTest(TestCase):
+    """
+    Tests that new items and lists can be create
+    """
+
+    def test_can_save_a_POST_request(self):
+        """
+        tests that the homepage can save a POST request
+        """
+        item_text = 'A new list item'
+        response = self.client.post('/lists/new', data={'item_text': item_text})
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, item_text)
+
+    def test_redirect_after_a_POST(self):
+        """
+        tests that the view redirects after it did send a POST request
+        """
+        item_text = 'A new list item'
+        response = self.client.post('/lists/new', data={'item_text': item_text})
+        self.assertRedirects(response, '/lists/the-only-list-in-the-world/')
