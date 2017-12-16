96f1d65c15520ce8018782f5ad64530abd88a8c6
Benjamin <benjamin.bein.phd@gmail.com>
2017-12-16 16:25:48 -0500

    fixed bugs in the deployment script

---
+++ deploy_tools/fabfile.py
@@ -0,0 +1,81 @@
+"""
+Fabric file to deploy the source code to the web and get all files into the correct places.
+"""
+
+import random
+
+from fabric.contrib.files import append, exists, sed
+from fabric.api import env, local, run
+
+REPO_URL = 'https://github.com/bbein/tdd-lists.git'
+
+def deploy():
+    """
+    Deployes the env.host webapplication
+    """
+    site_folder = f'/home/{env.user}/sites/{env.host}'
+    source_folder = site_folder + '/source'
+    _create_directory_structure_if_necessary(site_folder)
+    _get_lastest_source(source_folder)
+    _update_settings(source_folder, env.host)
+    _update_virtualenv(source_folder)
+    _update_static_files(source_folder)
+    _update_database(source_folder)
+
+def _create_directory_structure_if_necessary(site_folder):
+    """
+    creates the necessary folder structure for the website if it doesn't already exist
+    """
+    for sub_folder in ('database', 'static', 'virtualenv', 'source'):
+        run(f'mkdir -p {site_folder}/{sub_folder}')
+
+def _get_lastest_source(source_folder):
+    """
+    fetches the latest code of all branches.
+    If no .git folder exists it will clone the git repo.
+    In the end he server is set to te commit that is crrently checkedout locally.
+    """
+    if exists(source_folder + '/.git'):
+        run(f'cd {source_folder} && git fetch')
+    else:
+        run(f'git clone {REPO_URL} {source_folder}')
+    current_commit = local('git log -n 1 --format=%H', capture=True)
+    run(f'cd {source_folder} && git reset --hard {current_commit}')
+
+def _update_settings(source_folder, site_name):
+    """
+    updates the allowed hosts and the secret key file.
+    """
+    settings_path = source_folder + '/superlists/settings.py'
+    sed(settings_path, 'DEBUG = True', 'DEBUG = False')
+    sed(settings_path,
+        'ALLOWED_HOSTS = .+$',
+        f'ALLOWED_HOSTS = ["{site_name}"]')
+    secret_key_file = source_folder + '/superlists/secret_key.py'
+    if not exists(secret_key_file):
+        chars = 'abcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*(-_=+)'
+        key = ''.join(random.SystemRandom().choice(chars) for _ in range(50))
+        append(secret_key_file, f'SECRET_KEY = "{key}"')
+    append(settings_path, '\nfrom .secret_key import SECRET_KEY')
+
+def _update_virtualenv(source_folder):
+    """
+    updates the virtualenv so all dependicies are installed
+    """
+    virtualenv_folder = source_folder + '/../virtualenv'
+    if not exists(virtualenv_folder + '/bin/pip'):
+        run(f'python3.6 -mvenv {virtualenv_folder}')
+    run(f'{virtualenv_folder}/bin/pip install -r {source_folder}/requierments.txt')
+
+def _update_static_files(source_folder):
+    """
+    collect all the needed static files
+    """
+    run(f'cd {source_folder} && ../virtualenv/bin/python manage.py collectstatic --noinput')
+
+def _update_database(source_folder):
+    """
+    migrate the database for the site
+    """
+    run(f'cd {source_folder} && ../virtualenv/bin/python manage.py migrate --noinput')
+
